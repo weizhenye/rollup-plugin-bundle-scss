5169e5c3085bffb1f047fc1fa5bd7caa1989d9a4
weizhenye <weizhenye@gmail.com>
2020-10-20 21:25:10 +0800

    0.1.2

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,23 @@
 'use strict';
 
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
-
 var fs = require('fs');
-var path = _interopDefault(require('path'));
-var compiler = _interopDefault(require('vue-template-compiler'));
+var path = require('path');
+var compiler = require('vue-template-compiler');
 var scssBundle = require('scss-bundle');
 
-function bundleScss({ output, exclusive = true } = {}) {
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var path__default = /*#__PURE__*/_interopDefaultLegacy(path);
+var compiler__default = /*#__PURE__*/_interopDefaultLegacy(compiler);
+
+function bundleScss({ output, exclusive = true, bundlerOptions = {} } = {}) {
   const files = [];
+  const {
+    project = null,
+    dedupeGlobs = [],
+    includePaths = [],
+    ignoreImports = [],
+  } = bundlerOptions;
   return {
     name: 'bundle-scss',
     transform(source, id) {
@@ -19,7 +28,7 @@ function bundleScss({ output, exclusive = true } = {}) {
         }
       }
       if (/\.vue$/.test(id)) {
-        const { styles } = compiler.parseComponent(source);
+        const { styles } = compiler__default['default'].parseComponent(source);
         files.push(
           ...styles
             .filter((style) => style.lang === 'scss')
@@ -32,16 +41,16 @@ function bundleScss({ output, exclusive = true } = {}) {
       return null;
     },
     async generateBundle(opts) {
-      const outputPath = path.resolve(
-        opts.file ? path.dirname(opts.file) : opts.dir,
-        output || `${opts.file ? path.parse(opts.file).name : 'index'}.scss`,
+      const outputPath = path__default['default'].resolve(
+        opts.file ? path__default['default'].dirname(opts.file) : opts.dir,
+        output || `${opts.file ? path__default['default'].parse(opts.file).name : 'index'}.scss`,
       );
-      await fs.promises.mkdir(path.dirname(outputPath), { recursive: true });
+      await fs.promises.mkdir(path__default['default'].dirname(outputPath), { recursive: true });
       const entryContent = files.map((file) => `@import "${file.id}";`).join('\n');
       await fs.promises.writeFile(outputPath, entryContent);
       const registry = Object.assign({}, ...files.map((file) => ({ [file.id]: file.content })));
-      const bundler = new scssBundle.Bundler(registry);
-      const result = await bundler.bundle(outputPath);
+      const bundler = new scssBundle.Bundler(registry, project);
+      const result = await bundler.bundle(outputPath, dedupeGlobs, includePaths, ignoreImports);
       await fs.promises.writeFile(outputPath, result.bundledContent);
     },
   };
